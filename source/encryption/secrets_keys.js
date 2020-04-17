5fb60a70d09b4f6a6949630c02a072e79b86f579
Richard Snider <richardnsnider@gmail.com>
2020-04-17 04:35:41 +0000

    add kms region provider options

--- source/encryption/secrets_keys.js
+++ source/encryption/secrets_keys.js
@@ -1,5 +1,6 @@
 const aws = require('@pulumi/aws');
+const providers = require('../providers');
 
-module.exports.secrets_us_east_1 = new aws.kms.Key(`secrets_us_east_1`, {});
-module.exports.secrets_us_west_2 = new aws.kms.Key(`secrets_us_west_2`, {});
-module.exports.secrets_ap_south_1 = new aws.kms.Key(`secrets_ap_south_1`, {});
+module.exports.secrets_us_east_1 = new aws.kms.Key(`secrets_us_east_1`, {}, providers.us_east_1);
+module.exports.secrets_us_west_2 = new aws.kms.Key(`secrets_us_west_2`, {}, providers.us_west_2);
+module.exports.secrets_ap_south_1 = new aws.kms.Key(`secrets_ap_south_1`, {}, providers.ap_south_1);
