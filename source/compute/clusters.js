49ca612947e00445b9978e739171e7ecf6f8f8fb
Richard Snider <richardnsnider@gmail.com>
2020-04-17 04:35:04 +0000

    add clusters to other regions

--- source/compute/clusters.js
+++ source/compute/clusters.js
@@ -1,6 +1,15 @@
 const aws = require('@pulumi/aws');
+const providers = require('../providers');
 const cluster_roles = require('../iam/cluster_roles');
 
 module.exports.us_west_2 = new aws.eks.Cluster(`us_west_2`, {
   roleArn: cluster_roles.standard_cluster.arn
-});
+}, providers.us_west_2);
+
+module.exports.us_east_1 = new aws.eks.Cluster(`us_east_1`, {
+  roleArn: cluster_roles.standard_cluster.arn
+}, providers.us_east_1);
+
+module.exports.ap_south_1 = new aws.eks.Cluster(`ap_south_1`, {
+  roleArn: cluster_roles.standard_cluster.arn
+}, providers.ap_south_1);
