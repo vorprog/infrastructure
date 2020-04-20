2ec9a9e1ab18dda1b4565974b87df7e8ea9615cf
Richard Snider <richardnsnider@gmail.com>
2020-04-20 17:03:37 +0000

    alter vorprog internal dns name

--- source/dns/zones.js
+++ source/dns/zones.js
@@ -2,4 +2,4 @@ const aws = require('@pulumi/aws');
 
 module.exports.vorprog = new aws.route53.Zone(`vorprog.com`, {});
 
-module.exports.internal_vorprog = new aws.route53.Zone(`internal-vorprog.com`, {});
+module.exports.internal_vorprog = new aws.route53.Zone(`internal.vorprog.com`, {});
