98900c430a735aa2dd3fd6161eb0174a445769db
Richard Snider <richardnsnider@gmail.com>
2020-04-20 23:42:38 +0000

    fix cluster role policy setup

--- source/iam/cluster_roles.js
+++ source/iam/cluster_roles.js
@@ -1,7 +1,9 @@
 const aws = require('@pulumi/aws');
 
-module.exports.standard_cluster = new aws.iam.Role(`standard_cluster`, {
-  assumeRolePolicy: JSON.stringify({
+module.exports.standard_cluster = new aws.iam.Role(`standard_cluster`, {});
+
+module.exports.standard_cluster_policy = new aws.iam.Policy(`standard_cluster`, {
+  policy: JSON.stringify({
     Version: `2012-10-17`,
     Statement: [
       {
@@ -11,4 +13,9 @@ module.exports.standard_cluster = new aws.iam.Role(`standard_cluster`, {
       }
     ]
   })
+});
+
+module.exports.standard_cluster_attachment = new aws.iam.PolicyAttachment(`standard_cluster`, {
+  policyArn: this.standard_cluster_policy.arn,
+  roles: [ this.standard_cluster ]
 });
