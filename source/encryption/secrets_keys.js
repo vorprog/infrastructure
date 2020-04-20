7595d5f54327854dfd86e58afef6117cb68224e2
Richard Snider <richardnsnider@gmail.com>
2020-04-20 17:03:21 +0000

    custom resource options fix

--- source/encryption/secrets_keys.js
+++ source/encryption/secrets_keys.js
@@ -1,6 +1,6 @@
 const aws = require('@pulumi/aws');
-const providers = require('../providers');
+const options = require('../options');
 
-module.exports.secrets_us_east_1 = new aws.kms.Key(`secrets_us_east_1`, {}, providers.us_east_1);
-module.exports.secrets_us_west_2 = new aws.kms.Key(`secrets_us_west_2`, {}, providers.us_west_2);
-module.exports.secrets_ap_south_1 = new aws.kms.Key(`secrets_ap_south_1`, {}, providers.ap_south_1);
+module.exports.secrets_us_west_2 = new aws.kms.Key(`secrets_us_west_2`, {}, options.us_west_2);
+module.exports.secrets_us_east_1 = new aws.kms.Key(`secrets_us_east_1`, {}, options.us_east_1);
+module.exports.secrets_ap_south_1 = new aws.kms.Key(`secrets_ap_south_1`, {}, options.ap_south_1);
