435d6e0e9cb09197577685768754ef33a01f3447
Richard Snider <richardnsnider@gmail.com>
2020-04-21 05:20:55 +0000

    explicitly set provider options on s3 buckets

--- source/storage/buckets.js
+++ source/storage/buckets.js
@@ -1,13 +1,14 @@
 const aws = require('@pulumi/aws');
+const options = require('../options');
 
-module.exports.pulumi_config = new aws.s3.Bucket(`pulumi_config`, {
+module.exports.vorprog_pulumi_state_files = new aws.s3.Bucket(`vorprog_pulumi_state_files`, {
   region: aws.USWest2Region.toString()
-});
+}, options.us_west_2);
 
-module.exports.cdn = new aws.s3.Bucket(`cdn`, {
+module.exports.vorprog_cdn = new aws.s3.Bucket(`vorprog_cdn`, {
   region: aws.USWest2Region.toString()
-});
+}, options.us_west_2);
 
-module.exports.vorprog_kubernetes_cluster_us_west_2 = new aws.s3.Bucket(`vorprog_kubernetes_cluster_us_west_2`, {
+module.exports.vorprog_kubernetes_cluster_configs = new aws.s3.Bucket(`vorprog_kubernetes_cluster_configs`, {
   region: aws.USWest2Region.toString()
-});
+}, options.us_west_2);
