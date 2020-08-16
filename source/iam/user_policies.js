9ef9220e824986e5fc5553575bd689693e057a25
Richard Snider <richardnsnider@gmail.com>
2020-08-16 01:48:42 +0000

    add mfa user policy

--- source/iam/user_policies.js
+++ source/iam/user_policies.js
@@ -1,6 +1,7 @@
 const aws = require('@pulumi/aws');
 const users = require('./users');
 const roles = require('./roles');
+const map = require('lodash.map');
 
 const assumeRolePolicyJson = (roleArn) => JSON.stringify({
   Version: `2012-10-17`,
@@ -23,4 +24,70 @@ module.exports.developers_read_only_attachment = new aws.iam.PolicyAttachment(`d
       users.user_1,
       users.user_2
     ]
+});
+
+module.exports.mfa = new aws.iam.Policy(`mfa`, {
+  policy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: [
+          'iam:UpdateLoginProfile',
+          'iam:DeleteVirtualMFADevice',
+          'iam:EnableMFADevice',
+          'iam:ResyncMFADevice',
+          'iam:DeactivateMFADevice',
+          'iam:UploadSigningCertificate',
+          'iam:DeleteAccessKey',
+          'iam:GetSSHPublicKey',
+          'iam:UpdateAccessKey',
+          'iam:DeleteSSHPublicKey',
+          'iam:CreateVirtualMFADevice',
+          'iam:UpdateSSHPublicKey',
+          'iam:ListSigningCertificates',
+          'iam:CreateAccessKey',
+          'iam:CreateLoginProfile',
+          'iam:ListSSHPublicKeys',
+          'iam:DeleteSigningCertificate',
+          'iam:UploadSSHPublicKey',
+          'iam:UpdateSigningCertificate',
+          'iam:GetLoginProfile',
+          'iam:DeleteLoginProfile',
+          'iam:ChangePassword',
+          'iam:ListAccessKeys',
+          'iam:ListUsers'
+        ],
+        Resource: [
+          'arn:aws:iam::*:mfa/${aws:username}',
+          'arn:aws:iam::*:user/${aws:username}'
+        ]
+      },
+      {
+        Sid: 'AllowIndividualUserToListOnlyTheirOwnMFA',
+        Effect: 'Allow',
+        Action: [
+          'iam:ListMFADevices'
+        ],
+        Resource: [
+          'arn:aws:iam::*:mfa/*',
+          'arn:aws:iam::*:user/${aws:username}'
+        ]
+      },
+      {
+        Sid: 'AllowListActions',
+        Effect: 'Allow',
+        Action: [
+          'iam:ListUsers',
+          'iam:ListVirtualMFADevices'
+        ],
+        Resource: '*'
+      }
+    ]
+  })
+});
+
+module.exports.mfa_attachment = new aws.iam.PolicyAttachment(`mfa`, {
+  policyArn: this.mfa.arn,
+  users: map(users)
 });
