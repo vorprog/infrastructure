9a795cee60855a5a9d18b8369179c8d19513b02e
Richard Snider <richardnsnider@gmail.com>
2020-08-24 15:42:28 +0000

    run script cleanup

--- run.sh
+++ run.sh
@@ -9,13 +9,12 @@ read -s CLOUD_INFRASTRUCTURE_SECRET_ACCESS_KEY
 echo
 
 docker run \
---name cloud-infrastructure-create \
---interactive \
---tty \
 --env AWS_ACCESS_KEY_ID=$CLOUD_INFRASTRUCTURE_ACCESS_KEY_ID \
 --env AWS_SECRET_ACCESS_KEY=$CLOUD_INFRASTRUCTURE_SECRET_ACCESS_KEY \
+--env ORGANIZATION_NAME=vorprog \
 --env AWS_DEFAULT_REGION=us-west-2 \
--- env AWS_REGION_2=us-east-1 \
--- env ORGANIZATION_NAME=vorprog \
+--env AWS_SECONDARY_REGION=us-east-1 \
+--interactive \
+--tty \
 --entrypoint /bin/bash node source/main.js $1
 cloud-infrastructure:latest # TODO: reference remote image url from docker hub
