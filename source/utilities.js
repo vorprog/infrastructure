085145f7b7d58decb4f72fb8c9ce6a3460d0cc5b
Richard Snider <richardnsnider@gmail.com>
2020-08-27 19:54:27 +0000

    fix utilities tweak

--- source/utilities.js
+++ source/utilities.js
@@ -15,12 +15,18 @@ module.exports = {
     log(new Error().stack.split(`at `)[2].trim()); //trace
     log(command);
 
-    const options = {stdio : 'pipe' };
-    const result = childProcess.execSync(command, options);
-    log(`Result: ${result.toString()}`);
+    const options = { stdio: 'pipe' };
+    const result = childProcess.execSync(command, options).toString();
+    log(result);
+
     return result;
   },
   sleep: util.promisify(setTimeout),
   getObjectFromJsonFile: (path) => JSON.parse(fs.readFileSync(path).toString()),
   writeObjectToFile: (path, object) => fs.writeFileSync(path, JSON.stringify(object)),
 };
+
+// TODO: jsdoc notation for intellisense
+Object.defineProperty(String.prototype, "parseJson", {
+  value: function () { return JSON.parse(this); },
+});
