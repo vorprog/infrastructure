5513b5e29873727d1711608172566b302f76480d
Richard Snider <richardnsnider@gmail.com>
2020-09-24 14:53:01 +0000

    simplify and fix exec

--- source/utilities/exec.js
+++ source/utilities/exec.js
@@ -1,35 +1,27 @@
 const childProcess = require('child_process');
-const fs = require('fs');
-const map = require('lodash.map');
-
-const timestamp = `blaaaaaaaa`; // TODO
-
-const log = (content) => {
-  console.log(content);
-  fs.appendFileSync(`./${timestamp}.log`, content);
-}
+const each = require('lodash.map');
 
 const ignorableErrors = [
-  // TODO: clarify common error messages for already existing resources
-  `already exists`
+  `EntityAlreadyExists`,
 ];
 
 module.exports = command => {
-  log(`${new Error().stack.split(`at `)[2].trim()}$ ${command}`); //trace
+  console.log(`\nTRACE: ${new Error().stack.split(`at `)[2].replace(`Object.<anonymous>`,``).trim()}`);
+  console.log(`$ ${command}`);
 
   const options = { stdio: 'pipe' };
   let result;
 
   try {
     result = childProcess.execSync(command, options).toString();
+    if (result) {
+      console.log(result);
+      return result;
+    }
   } catch (err) {
     errorMessage = err.stderr.toString();
-    map(ignorableErrors, message => {
-      if(errorMessage.includes(message)) log(errorMessage);
-      else throw new Error(errorMessage);
+    each(ignorableErrors, message => {
+      if (!errorMessage.includes(`An error occurred (${message})`)) throw new Error(errorMessage);
     });
   }
-
-  log(result);
-  return result;
 };
