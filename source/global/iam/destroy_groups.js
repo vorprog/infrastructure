4a8e24c23d5e8f6c6524d0ec2eec1d1a8224d274
Richard Snider <richardnsnider@gmail.com>
2020-09-24 14:52:05 +0000

    fix destroy iam groups and users scripts

--- source/global/iam/destroy_groups.js
+++ source/global/iam/destroy_groups.js
@@ -1,11 +1,11 @@
 const exec = require('../../utilities/exec');
 
-exec(`aws iam unassign-group --name rsnider --group-name administrators`);
-exec(`aws iam delete-group-policy --group-name administrators --policy-document file:///root/source/global/iam/policies/administrator.json --policy-name administrator`);
-exec(`aws iam delete-group-policy --group-name administrators --policy-document file:///root/source/global/iam/policies/mfa_requirement.json --policy-name mfa_requirement`);
+exec(`aws iam remove-user-from-group --user-name rsnider --group-name administrators`);
+exec(`aws iam delete-group-policy --group-name administrators --policy-name administrator`);
+exec(`aws iam delete-group-policy --group-name administrators --policy-name mfa_requirement`);
 exec(`aws iam delete-group --group-name administrators`);
 
-exec(`aws iam unassign-group --name mroberts --group-name developers`);
-exec(`aws iam delete-group-policy --group-name developers --policy-document file:///root/source/global/iam/policies/read_only.json --policy-name read_only`);
-exec(`aws iam delete-group-policy --group-name developers --policy-document file:///root/source/global/iam/policies/mfa_requirement.json --policy-name mfa_requirement`);
+exec(`aws iam remove-user-from-group --user-name mroberts --group-name developers`);
+exec(`aws iam delete-group-policy --group-name developers --policy-name read_only`);
+exec(`aws iam delete-group-policy --group-name developers --policy-name mfa_requirement`);
 exec(`aws iam delete-group --group-name developers`);
--- source/global/iam/destroy_users.js
+++ source/global/iam/destroy_users.js
@@ -1,4 +1,7 @@
 const exec = require('../../utilities/exec');
 
+exec(`aws iam delete-login-profile --user-name rsnider`);
 exec(`aws iam delete-user --user-name rsnider`);
+
+exec(`aws iam delete-login-profile --user-name mroberts`);
 exec(`aws iam delete-user --user-name mroberts`);
