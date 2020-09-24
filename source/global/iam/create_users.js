7d57324ff9ee5139f8b1d761e3a51dcb7d723e4a
Richard Snider <richardnsnider@gmail.com>
2020-09-24 14:51:13 +0000

    create logins for users

--- source/global/iam/create_users.js
+++ source/global/iam/create_users.js
@@ -1,4 +1,18 @@
 const exec = require('../../utilities/exec');
 
-exec(`aws iam create-user --name rsnider`);
-exec(`aws iam create-user --name mroberts`);
+generatePassword = (length) => {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+  for (let i = 0; i < length; ++i) {
+    let randomNumber = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomNumber);
+  }
+  return password;
+}
+
+exec(`aws iam create-user --user-name rsnider`);
+exec(`aws iam create-login-profile --user-name rsnider --password ${generatePassword(10)} --password-reset-required`);
+
+exec(`aws iam create-user --user-name mroberts`);
+exec(`aws iam create-login-profile --user-name mroberts --password ${generatePassword(10)} --password-reset-required`);
+
