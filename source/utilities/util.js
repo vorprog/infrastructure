f18fef3df151180fed625626ca2d82560e50d527
Richard Snider <richardnsnider@gmail.com>
2020-10-01 16:05:00 +0000

    findbynametag function on utilities

--- source/utilities/util.js
+++ source/utilities/util.js
@@ -1,8 +1,13 @@
+const find = require('lodash.find');
+
+const getNameTag = resource => find(resource.Tags, { Key: `Name` }).Value;
+
 module.exports = {
   exec: require('./exec'),
-  state: require('./state'),
   file: require('./file'),
-  each: require('lodash.map'),
-  first: (collection, predicate) => require('lodash.filter')(collection, predicate)[0],
   generateSecret: require('./generate_secret'),
+  each: require('lodash.map'),
+  filter: require('lodash.filter'),
+  first: (collection, predicate) => find(collection, predicate),
+  findByNameTag: (collection, name) => find(collection, item => getNameTag(item) === name),
 };
