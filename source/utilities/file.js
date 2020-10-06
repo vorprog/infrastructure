e0078d03194f8a06b43ec1308b3799de288140eb
Richard Snider <richardnsnider@gmail.com>
2020-10-06 19:28:02 +0000

    add yaml file operations

--- source/utilities/file.js
+++ source/utilities/file.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 module.exports = {
   writeJson: (data, path) => fs.writeFileSync(path, JSON.stringify(data)),
-  getJson: (path) => JSON.parse(fs.readFileSync(path).toString())
+  getJson: (path) => JSON.parse(fs.readFileSync(path).toString()),
+  writeYaml: (data, path) => fs.writeFileSync(path, yaml.dump(data)),
+  getYaml: (path) => yaml.load(fs.readFileSync(path).toString())
 };
