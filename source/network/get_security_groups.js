e42eb6c916adf544c7c7e1f50a936d33daae7a99
Richard Snider <richardnsnider@gmail.com>
2020-10-06 20:23:43 +0000

    fix security groups getter

--- source/network/get_security_groups.js
+++ source/network/get_security_groups.js
@@ -5,10 +5,10 @@ const securityGroups1 = exec(`aws ec2 describe-security-groups --region ${proces
 const securityGroups2 = exec(`aws ec2 describe-security-groups --region ${process.env.AWS_SECONDARY_REGION}`).SecurityGroups;
 
 const privateNetwork1 = util.first(securityGroups1, { GroupName: `private-network`});
-const privateNetwork2 = util.first(securityGroups1, { GroupName: `private-network`});
+const privateNetwork2 = util.first(securityGroups2, { GroupName: `private-network`});
 
 const httpInternet1 = util.first(securityGroups1, { GroupName: `http-internet`});
-const httpInternet2 = util.first(securityGroups1, { GroupName: `http-internet`});
+const httpInternet2 = util.first(securityGroups2, { GroupName: `http-internet`});
 
 module.exports = {
   privateNetwork1: privateNetwork1,
