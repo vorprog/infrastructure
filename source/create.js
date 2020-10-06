cb7fcdb530619fd0cff16572448a9d31a88fc4bb
Richard Snider <richardnsnider@gmail.com>
2020-10-06 20:41:41 +0000

    update full create script

--- source/create.js
+++ source/create.js
@@ -1,2 +1,13 @@
 require('./iam/create_users');
 require('./iam/create_groups');
+
+require('./network/create_vpcs');
+require('./network/create_private_networks');
+require('./network/create_public_networks');
+
+require('./storage/create_buckets');
+require('./storage/create_keys');
+
+require('./iam/create_cluster_admin_role');
+require('./compute/create_clusters');
+require('./compute/create_cluster_roles');
