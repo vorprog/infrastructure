f7b708e573967ce99434f8aa31287ef6f2305e83
Richard Snider <richardnsnider@gmail.com>
2020-10-06 20:27:47 +0000

    create iam role for cluster admin

---
+++ source/iam/create_cluster_admin_role.js
@@ -0,0 +1,23 @@
+const exec = require('../utilities/exec');
+
+const accountId = exec(`aws sts get-caller-identity`).Account;
+const assumeRolePolicy = {
+  Version: `2012-10-17`,
+  Statement: [
+    {
+      Effect: `Allow`,
+      Principal: {
+        AWS: `arn:aws:iam::${accountId}:root`
+      },
+      Action: `sts:AssumeRole`,
+      Condition: {}
+    }
+  ]
+};
+
+exec(`aws iam create-role
+--role-name ClusterAdmin
+--description "Kubernetes administrator role (for AWS IAM Authenticator for AWS)."
+--assume-role-policy-document "${JSON.stringify(assumeRolePolicy)}"
+--output text
+--query 'Role.Arn'`);
