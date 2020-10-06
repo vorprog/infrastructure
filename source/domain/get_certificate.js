b8e6dabca54d283bb671c80d7232e0c462e18a11
Richard Snider <richardnsnider@gmail.com>
2020-10-06 19:28:42 +0000

    certificate getter

---
+++ source/domain/get_certificate.js
@@ -0,0 +1,7 @@
+const exec = require('../utilities/exec');
+const util = require('../utilities/util');
+
+const certificates = exec(`aws acm list-certificates`).CertificateSummaryList;
+const domainCertificate = util.first(certificates, { DomainName: process.env.DOMAIN_NAME });
+
+module.exports = domainCertificate;
