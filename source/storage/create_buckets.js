928c079761056739f34856949639d67c7c9d1c2e
Richard Snider <richardnsnider@gmail.com>
2020-10-08 23:20:02 +0000

    add encryption to s3 buckets

--- source/storage/create_buckets.js
+++ source/storage/create_buckets.js
@@ -1,6 +1,18 @@
 const exec = require('../utilities/exec');
-const region1 = process.env.AWS_DEFAULT_REGION;
-const region2 = process.env.AWS_SECONDARY_REGION;
 
-exec(`aws s3api create-bucket --region ${region1} --bucket ${process.env.DOMAIN_NAME}-cluster1-kops-state --create-bucket-configuration LocationConstraint=${region1}`);
-exec(`aws s3api create-bucket --region ${region2}  --bucket ${process.env.DOMAIN_NAME}-cluster2-kops-state --create-bucket-configuration LocationConstraint=${region2}`);
+exec(`aws s3api create-bucket --region ${process.env.AWS_DEFAULT_REGION} --bucket ${process.env.DOMAIN_NAME}-kops-state 
+--create-bucket-configuration LocationConstraint=${process.env.AWS_DEFAULT_REGION} --query 'Bucket'`);
+
+const encryptionConfiguration = JSON.stringify({
+  Rules: [
+    {
+      ApplyServerSideEncryptionByDefault:
+      {
+        SSEAlgorithm: `AES256`
+      }
+    }
+  ]
+});
+
+exec(`aws s3api put-bucket-encryption --bucket ${process.env.DOMAIN_NAME}-kops-state
+--server-side-encryption-configuration '${encryptionConfiguration}'`);
