b259e7c8362b2f2f99e1a9c4bbdac96d1912025c
Richard Snider <richardnsnider@gmail.com>
2020-10-08 20:44:30 +0000

    full create script tweaks

--- source/create.js
+++ source/create.js
@@ -1,13 +1,16 @@
+require('./storage/create_keys');
+
 require('./iam/create_users');
 require('./iam/create_groups');
 
 require('./network/create_vpcs');
+require('./network/create_subnets');
+require('./network/create_transit_gateway');
 require('./network/create_private_networks');
 require('./network/create_public_networks');
 
 require('./storage/create_buckets');
-require('./storage/create_keys');
 
-require('./iam/create_cluster_admin_role');
 require('./compute/create_clusters');
+require('./iam/create_cluster_admin_role');
 require('./compute/create_cluster_roles');
