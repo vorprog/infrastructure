1d4b9dbe6d311a42be647493504eaaf739968c7f
Richard Snider <richardnsnider@gmail.com>
2020-10-08 23:18:41 +0000

    add query option in kms create keys script

--- source/storage/create_keys.js
+++ source/storage/create_keys.js
@@ -3,14 +3,14 @@ const exec = require('../utilities/exec');
 const region1 = process.env.AWS_DEFAULT_REGION;
 const region2 = process.env.AWS_SECONDARY_REGION;
 
-const symmetric1 = exec(`aws kms create-key --region ${region1}`).KeyMetadata;
-const symmetric2 = exec(`aws kms create-key --region ${region2}`).KeyMetadata;
+const symmetric1 = exec(`aws kms create-key --region ${region1} --query KeyMetadata`)
+const symmetric2 = exec(`aws kms create-key --region ${region2} --query KeyMetadata`)
 
 exec(`aws kms create-alias --region ${region1} --alias-name symmetric --target-key-id ${symmetric1.KeyId}`);
 exec(`aws kms create-alias --region ${region2} --alias-name symmetric --target-key-id ${symmetric2.KeyId}`);
 
-const asymmetric1 = exec(`aws kms create-key --region ${region1} --customer-master-key-spec RSA_4096`).KeyMetadata;
-const asymmetric2 = exec(`aws kms create-key --region ${region2} --customer-master-key-spec RSA_4096`).KeyMetadata;
+const asymmetric1 = exec(`aws kms create-key --region ${region1} --customer-master-key-spec RSA_4096 --query KeyMetadata`)
+const asymmetric2 = exec(`aws kms create-key --region ${region2} --customer-master-key-spec RSA_4096 --query KeyMetadata`)
 
 exec(`aws kms create-alias --region ${region1} --alias-name asymmetric --target-key-id ${asymmetric1.KeyId}`);
 exec(`aws kms create-alias --region ${region2} --alias-name asymmetric --target-key-id ${asymmetric2.KeyId}`);
