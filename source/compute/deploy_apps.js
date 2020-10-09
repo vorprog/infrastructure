3b478a2738197c4675fb134bcb6bef253b0b9887
Richard Snider <richardnsnider@gmail.com>
2020-10-09 15:49:53 +0000

    improve deploy app script to include secrets

--- source/compute/deploy_apps.js
+++ source/compute/deploy_apps.js
@@ -1,11 +1,22 @@
 const exec = require('../utilities/exec');
 const util = require('../utilities/util');
 
-const appRepositories = [];
+const appRepositories = []; // TODO
 
 util.each(appRepositories, repoUrl => {
-  exec(`curl "${repoUrl}" -o current_app`);
-  exec(`unzip current_app.zip`)
-  exec(`kubectl apply -f current_app/manifests/production`);
-  exec(`rm -rf current_app`);
+  // Download production manifests directory
+  exec(`git clone ${repoUrl} --single-branch --branch master --no-checkout source/iam --depth 1 current_app_repo`);
+  exec(`git sparse-checkout init --cone`); // to fetch only root files
+  exec(`git sparse-checkout set manifests/production`);
+
+  // Decrypt and apply secrets
+  const secretFiles = exec(`ls current_app_repo/manifests/production/secrets`).split(`\n`);
+  util.each(secretFiles, secretFile => {
+    exec(`sops --decrypt current_app_repo/manifests/production/secrets/${secretFile} | kubectl apply --filename -`);
+  });
+  exec(`rm -rf current_app_repo/manifests/production/secrets`);
+  
+  // Apply all other app resources
+  exec(`kubectl apply --filename current_app_repo/manifests/production`);
+  exec(`rm -rf current_app_repo`);
 });
