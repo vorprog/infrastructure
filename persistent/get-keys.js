459fb0608aaf2a184c07b31e19ad57a560c83afd
Richard Snider <richardnsnider@gmail.com>
2021-12-30 15:31:13 -0800

    add new script for getting kms keys

---
+++ persistent/get-keys.js
@@ -0,0 +1,21 @@
+const exec = require('../utilities/exec');
+
+const symmetric1 = exec(`aws kms describe-keys --region ${process.env.AWS_DEFAULT_REGION} --filter 'Alias=symmetric' --query 'KmsKeys[0]'`);
+const symmetric2 = exec(`aws kms describe-keys --region ${process.env.AWS_SECONDARY_REGION} --filter 'Alias=symmetric' --query 'KmsKeys[0]'`);
+
+const asymmetric1 = exec(`aws kms describe-keys --region ${process.env.AWS_DEFAULT_REGION} --filter 'Alias=asymmetric' --query 'KmsKeys[0]'`);
+const asymmetric2 = exec(`aws kms describe-keys --region ${process.env.AWS_SECONDARY_REGION} --filter 'Alias=asymmetric' --query 'KmsKeys[0]'`);
+
+asymmetric1.PublicKey = exec(`aws kms get-public-key --key-id ${symmetric1.KeyId} --output text --query 'PublicKey' | base64 --decode`);
+asymmetric2.PublicKey = exec(`aws kms get-public-key --key-id ${symmetric2.KeyId} --output text --query 'PublicKey' | base64 --decode`);
+
+module.exports = {
+  region1: {
+    symmetric: symmetric1,
+    asymmetric: asymmetric1
+  },
+  region2: {
+    symmetric: symmetric2,
+    asymmetric: asymmetric2
+  }
+};
